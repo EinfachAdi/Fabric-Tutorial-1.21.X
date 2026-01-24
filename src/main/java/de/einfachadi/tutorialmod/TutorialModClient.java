5fdb38e34670f0b315242c5580e4e36039005a9b
EinfachAdi <adrianmattickdavid@gmail.com>
2026-01-24 21:32:14 +0100

    Initial commit

---
+++ src/main/java/de/einfachadi/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package de.einfachadi.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
